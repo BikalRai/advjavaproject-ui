1e857fd3fdabf9dca82288482fb6a081c47cb0f8
Bikal Rai <bikalrai2785@gmail.com>
2024-09-12 13:19:26 +0545

    initiate display venue and timeslot for venue

--- src/components/pages/venues/UserVenueTime.jsx
+++ src/components/pages/venues/UserVenueTime.jsx
@@ -1,38 +1,80 @@
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-} from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./venueDetails.scss";
 
 const UserVenueTime = () => {
+  const [venueDetails, setVenueDetails] = useState({});
+  const [timeslots, setTimeSlots] = useState([]);
+
+  const getAllVenueData = async (id) => {
+    try {
+      const venueRes = await axios.get(
+        `http://localhost:8080/api/venues/${id}`
+      );
+
+      setVenueDetails(venueRes.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const getAllVenueTimeSlots = async (id) => {
+    try {
+      const timeRes = await axios.get(`http://localhost:8080/api/time/${id}`);
+
+      setTimeSlots(timeRes.data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllVenueData(1);
+    getAllVenueTimeSlots(1);
+  }, []);
+
+  console.log(venueDetails);
+  console.log(timeslots);
   return (
-    <div>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography
-            gutterBottom
-            sx={{ color: "text.secondary", fontSize: 14 }}
-          >
-            Word of the Day
-          </Typography>
-          <Typography variant='h5' component='div'>
-            belent
-          </Typography>
-          <Typography sx={{ color: "text.secondary", mb: 1.5 }}>
-            adjective
-          </Typography>
-          <Typography variant='body2'>
-            well meaning and kindly.
-            <br />
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size='small'>Learn More</Button>
-        </CardActions>
-      </Card>
+    <div className='venue'>
+      <div className='venue__details container'>
+        <div className='venue__details--body'>
+          <h1 className='venue__details--body-title'>{venueDetails.name}</h1>
+          <p className='venue__details--body-text'>
+            <span>Description:</span> {venueDetails.description}
+          </p>
+          <p className='venue__details--body-text'>
+            <span>Location:</span>
+            {venueDetails.location}
+          </p>
+          <p className='venue__details--body-text'>
+            <span>Amenities: </span>
+            {venueDetails.amenities}
+          </p>
+          <p className='venue__details--body-text'>
+            <span>Opening Time:</span>
+            {venueDetails.openingTime}
+          </p>
+          <p className='venue__details--body-text'>
+            <span>Closing Time: </span>
+            {venueDetails.closingTime}
+          </p>
+          <p className='venue__details--body-text'>
+            <span>Price: </span>
+            {venueDetails.price}
+          </p>
+        </div>
+        <div className='venue__details--img'>
+          <img src={`data:image/jpeg;base64, ${venueDetails.image}`} alt='' />
+        </div>
+      </div>
+
+      <div className='venue__timeslots container '>
+        <h1>Available Time Slots</h1>
+        {/* {timeslots.map((timeslot) => (
+          
+        ))} */}
+      </div>
     </div>
   );
 };
